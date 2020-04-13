84db1bab51e393005e734ee9058c9900237ce960
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-04-13 19:53:26 +0200

    refactor function validator

--- practica03/js03.js
+++ practica03/js03.js
@@ -3,44 +3,84 @@
 
 function validator(pass){
 	var mayus = false;
+	var upper = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
 	var minus = false;
+	var lower = "abcdefghijklmnñopqrstuvwxyz";
+
 	var num = false;
+	var numbers ="1234567890";
+
 	var specialchar = false;
+
+	var cont = 0;
+	var cont2 = 0;
+	var cont3 = 0;
+	var cont4 = 0;
 
 	if(pass.length >= 8) {
 
+		/*Upper Case Section*/
 		for (var i = 0; i < pass.length; i++) {
-			if (pass.charCodeAt(i)>=65 && pass.charCodeAt(i)<=90 && pass.charCodeAt(i) == '\u00D1') {
-				//60 & 90 corresponding to ASCII number for uppercase chars
-				mayus = true;
-			}else if (pass.charCodeAt(i)>=97 && pass.charCodeAt(i)<=122&& pass.charCodeAt(i) == '\u00F1') {
-				minus = true;
-			}else if (pass.charCodeAt(i)>=48 && pass.charCodeAt(i)<=57) {
-				num = true;
-			}else{
-				specialchar = true;
+			for(var j= 0; j < upper.length; j++){
+				if(pass[i] == upper[j]){
+					cont += 1;
+				}
 			}
-
-			if (mayus == false || minus == false || num == false || specialchar == false) {
-				return "ok";
+		/*Lower Case Section*/
+			for (var l = 0; l < lower.length; l++) {
+				if (pass[i] == lower[l]) {
+					cont2 += 1;
+				}
+			}
+		/*Number Section*/
+			for(var k = 0; k < numbers.length; k++){
+				if(pass[i] == numbers[k]){
+					cont3 += 1;
+				}
+			}
+		/*Special chars Section*/
+			for(var m = 0; m < pass.length; m++){
+				if(pass.charCodeAt(m)>=48 && pass.charCodeAt(m)<=57){
+					cont4 += 1;
+				}
 			}
 		}
+	}else{
+		return("Error: password to short, minimun length 8");
 	}
 
-	if (pass.length <= 7) {
-		return("Error: password to short, minimun length 8");
+
+	if((cont == 0 || cont >= 2) && (cont2 == 0 || cont2 >= 2) && cont3 >= 0 && cont4 >= 0){
+			mayus = true;
+			minus =true;
+			numbers = true;
+			specialchar = true;
+	}
+
+	if (cont == 1) {
+		return("You need 2 or more Uppecase char you have " + cont);
+	}
+
+	if(cont2 == 1){
+		return("You need 2 or more Lowercase char you have " + cont2);
+	}
+	if(mayus == true && minus == true && numbers == true && specialchar == true){
+		return "ok";
 	}
 }
 
+
 var psswd = "123";
-var psswd1 = "12312345678976543245676";
+var psswd1 = "1231234567897654324567F";
 var psswd2 = "aaaaaaaaaaaaaaaaaaaaaaaaaaa";
-var psswd3 = "AAAAAAAAAAAAAAAAAAAAAAAAA"
+var psswd3 = "AAAAAAAAAAAAAAAAAAAAAAAAA";
 var psswd4 = "!·$%&/()=(/&%$·$%&/()($)|@#{{{}}})";
-var psswd5 = "12345asdc678asdf";
-var psswd6 = "dqwk,ksdAdfTfdsrgg";
+var psswd5 = "asdc678asFF";
+var psswd6 = "dqwk-.eflwe.";
 var psswd7 = "ññññññññÑÑÑÑÑÑÑÑÑÑÑ";
-var psswd8 = "ElgranCañóndelcolôrado"
+var psswd8 = "ñÑlGrancañóndelcolôrado"
+
 
 console.log(validator(psswd));
 console.log(validator(psswd1));
@@ -50,3 +90,4 @@ console.log(validator(psswd4));
 console.log(validator(psswd5));
 console.log(validator(psswd6));
 console.log(validator(psswd7));
+console.log(validator(psswd8));
