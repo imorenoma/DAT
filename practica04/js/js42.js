d22f02741f7196e8fcf66b907569a199ee93d37e
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-04-19 21:44:08 +0200

    refactor code, still not working

--- practica04/js/js42.js
+++ practica04/js/js42.js
@@ -1,34 +1,7 @@
-'use strict'
-function pasocm(m){
-    return(m*100);
-}
-function pasopulg(m){
-	return (m/0.0254);
-}
-function pasoyard(m){
-	 return (m/0.9144);
-}
-
-
-$("#cm").click(function() {
-	var v_text=$("#num").text();
-	var v_out= pasocm(v_text);
-	$("#v_out").text(v_out);
+$(document).ready(function(){
+   $("#cm").click(function() {
+      numero=$("#num").html();
+	  v_out = pasocm(numero);
+	  $("#v_out").text(v_out);
+   });
 });
-
-/*
-let num;
-
-num=4;
-console.log(pasocm(num));
-console.log(pasopulg(num));
-console.log(pasoyard(num));
-num=0.99;
-console.log(pasocm(num));
-console.log(pasopulg(num));
-console.log(pasoyard(num));
-num=-7;
-console.log(pasocm(num));
-console.log(pasopulg(num));
-console.log(pasoyard(num));
-*/
