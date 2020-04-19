e2e994bbb58007cacb453116e6358e21f3da2cb3
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-04-19 21:49:00 +0200

    change text in line 5 for html

--- practica04/js/js42.js
+++ practica04/js/js42.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
    $("#cm").click(function() {
       numero=$("#num").html();
 	  v_out = pasocm(numero);
-	  $("#v_out").text(v_out);
+	  $("#v_out").html(v_out);
    });
 });
