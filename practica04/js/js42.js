34fd7fab5a36db1a8e169332e9af670b6a6dff67
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-04-20 02:14:10 +0200

    code of js42,js complete and check

--- practica04/js/js42.js
+++ practica04/js/js42.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
-   $("#cm").click(function() {
-      numero=$("#num").html();
-	  v_out = pasocm(numero);
-	  $("#v_out").html(v_out);
-   });
+	$("#cm").click (function() {
+		let v_text=$("#v_in").text();
+        let v_out=pasocm(v_text)+ " cm";
+		$("#v_out").text(v_out);
+	});
+
+	$("#inch").click (function() {
+		let v_text=$("#v_in").text();
+        let v_out=pasopulg(v_text);
+		v_out = v_out.toFixed(2);
+		v_out = v_out + " in"
+		$("#v_out").text(v_out);
+	});
+
+	$("#yd").click (function() {
+		let v_text=$("#v_in").text();
+        let v_out=pasoyard(v_text);
+		v_out = v_out.toFixed(2);
+		v_out = v_out + " yd"
+		$("#v_out").text(v_out);
+	});
 });
