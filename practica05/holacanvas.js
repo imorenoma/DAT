83ba9b486272d66c8f77e41986e4ccfd106fd20b
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-05-08 11:40:44 +0200

    complete holacanvas

--- practica05/holacanvas.js
+++ practica05/holacanvas.js
@@ -9,17 +9,25 @@ function main(){
     let max_x = 100;
     let min_y = 0;
     vjcanvas.set_coords(min_x, max_x, min_y);
-	let colors = ['red', 'green', 'blue'];
+	let colors = ['grey', 'green', 'blue'];
 
     diametro = 5;
 
-    for (let x=0; x <= 600; x += diametro)
-        for (let y=0; y <= 300; y += diametro) {
+    for (let x=0; x <= 10; x += diametro)
+        for (let y=0; y <= 50; y += diametro) {
             vjcanvas.circle(x, y, diametro, colors[1]);
     }
+	for (let x = 15; x <=25; x += diametro)
+		for(let y=0; y <= 50; y += diametro){
+			vjcanvas.rectangle(x, y, 5, 5, colors[0]);
+		}
 
-	vjcanvas.rectangle(800, 0, 100, 200,colors[2]);
-	vjcanvas.rectangle(700, 0, 200, 200, colors[0]);
+	for (let x = 35; x <=45; x += diametro)
+		for(let y=0; y <= 50; y += diametro){
+			vjcanvas.rectangle(x, y, 5, 4,colors[2]);
+		}
+
+
 }
 
 main();
