14d6db87e7ef64930ea5146516c8967205c3fc04
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-05-10 15:57:14 +0200

    new function to the practice, now works japanise languaje in practice 4.4

--- practica04/js/cambiaBoton2.js
+++ practica04/js/cambiaBoton2.js
@@ -1,27 +1,72 @@
 $(document).ready(function() {
-	change = false;
-      $("#toggle-it").click(function() {
 
-		  if (change==false) {
+	var idiomas = {japOpen : "示す", japClose: "隠す" , ingOpen : "Show", ingClose :"Hide", spOpen : "Mostrar", spClose : "Ocultar"};
+	change = false;
+	jap = false;
+	gb = false;
+	esp=false;
+
+		$(".jap").click(function(){
+			jap = true;
+		});
+
+      $("#toggle-it").click(function() {
+		  if (change==false && jap==false && gb ==false) {
 		  	$("#image").show("oculto");
 			let v_text = $("#toggle-it").text();
-			let v_out = "Ocultar Foto";
+			let v_out = idiomas.spClose;
 			$("#toggle-it").text(v_out);
 			change=true;
-		}else{
+		}else if (change==false && jap==true && gb==false) {
+			$("#image").show("oculto");
+			let v_text = $("#toggle-it").text();
+			let v_out = idiomas.japClose;
+			$("#toggle-it").text(v_out);
+			change=true;
+		}else if (change==true && jap==true && gb==false) {
 			$("#image").hide("oculto");
 			let v_text = $("#toggle-it").text();
-			let v_out = "Mostrar Foto|
-			$("#toggle-it").text(v_out);
+			let v_out2 = idiomas.japOpen;
+			$("#toggle-it").text(v_out2);
+			change=false;
+		}else{
+
+			$("#image").hide("oculto");
+			let v_text = $("#toggle-it").text();
+			let v_out2 = idiomas.spOpen;
+			$("#toggle-it").text(v_out2);
 			change=false;
 		}
+	});
+});
 
-      });
 
-	 $(".jap").click(function(){
+
+
+/*
+$(".jap").click(function(){
+	if(change == false){
+	   let v_text = $("#toggle-it").text();
+	   let v_out = "示す";
+	   $("#toggle-it").text(v_out);
+	   change = true;
+   }else{
+	   let v_text = $("#toggle-it").text();
+	   let vout="隠す";
+	   $("#toggle-it").text(v_out);
+	   change = false;
+   }
+});
+
+	 $(".gb").click(function(){
 		 let v_text = $("#toggle-it").text();
-		 let v_out = "写真を見せて";
+		 let v_out = "Show";
 		 $("#toggle-it").text(v_out);
 	 });
 
-});
+	 $(".esp").click(function(){
+		 let v_text = $("#toggle-it").text();
+		 let v_out = "mostrar";
+		 $("#toggle-it").text(v_out);
+	 });
+	 */
