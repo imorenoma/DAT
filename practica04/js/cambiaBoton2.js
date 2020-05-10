4ce28758b7f93aeb97b72f71c05a5c74cbb1c2db
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-05-10 19:34:01 +0200

    table create, still not working

--- practica04/js/cambiaBoton2.js
+++ practica04/js/cambiaBoton2.js
@@ -2,34 +2,38 @@ $(document).ready(function() {
 
 	var idiomas = {japOpen : "示す", japClose: "隠す" , ingOpen : "Show", ingClose :"Hide", spOpen : "Mostrar", spClose : "Ocultar"};
 	change = false;
-	jap = false;
+	jP = false;
 	gb = false;
 	esp=true;
 
 	$(".jap").click(function(){
 		esp =false;
-		jap = true;
+		jP = true;
 		gb = false;
+		let texto = '<tr><td>' + moment().format() + '</td><td>' + "Configuramos el interface en Japones" + '</td></tr>';
+		$("#Registro").append(texto);
 	});
+
 	$(".ing").click(function(){
 		esp =false;
 		gb = true;
-		jap=false;
+		jP=false;
 	});
 
 	$(".esp").click(function(){
 		esp =true;
 		gb = false;
-		jap=false;
+		jP=false;
 	});
 
 	$("#toggle-it").click(function() {
+
 
 		/*El idioma por defecto es el español, ademas tambien podemos volver
 		a el dando click en la imagen del idioma. Para este caso cambiamos
 		el boton a ocultar y mostramos la imagen*/
 
-		if (change==false && jap==false && gb ==false && esp==true) {
+		if (change==false && jP==false && gb ==false && esp==true) {
 	  		$("#image").show("oculto");
 			let v_text = $("#toggle-it").text();
 			let v_out = idiomas.spClose;
@@ -41,33 +45,37 @@ $(document).ready(function() {
 /*-------------------------------------------------------*/
 
 		/*cambio boton a ocultar en japones y mostramos la imagen*/
-		}else if (change==false && jap==true && gb==false && esp==false) {
+		}else if (change==false && jP==true && gb==false && esp==false) {
 			$("#image").show("oculto");
 			let v_text = $("#toggle-it").text();
 			let v_out = idiomas.japClose;
 			$("#toggle-it").text(v_out);
 			change=true;
+
 		/*cambio boton a mostrar en japones y ocultamos la imagen*/
-		}else if (change==true && jap==true && gb==false && esp==false) {
+		}else if (change==true && jP==true && gb==false && esp==false) {
 			$("#image").hide("oculto");
 			let v_text = $("#toggle-it").text();
 			let v_out2 = idiomas.japOpen;
+			let texto;
 			$("#toggle-it").text(v_out2);
 			change=false;
+			texto = '<tr><td>' + moment().format() + '</td><td>'+"Cambio idioma a Japones"+'</td></tr>';
+			$("#Registro").append(texto);
 
 /*-------------------------------------------------------*/
 /*--------------------------INGLES------------------------*/
 /*-------------------------------------------------------*/
 
 		/*cambio boton a ocultar en ingles y mostramos la imagen*/
-		}else if (change == false && jap==false && gb == true && esp==false) {
+		}else if (change == false && jP==false && gb == true && esp==false) {
 			$("#image").show("oculto");
 			let v_text = $("#toggle-it").text();
 			let v_out = idiomas.ingClose;
 			$("#toggle-it").text(v_out);
 			change=true;
 		/*cambio boton a mostrar en ingles y ocultamos la imagen*/
-		}else if (change == true && jap==false && gb == true && esp==false) {
+		}else if (change == true && jP==false && gb == true && esp==false) {
 			$("#image").hide("oculto");
 			let v_text = $("#toggle-it").text();
 			let v_out2 = idiomas.ingOpen;
